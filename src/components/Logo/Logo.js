fd5d7846532fbf60959813a0193c5daec18801cc
DanieleTursi <99504866+DanieleTursi@users.noreply.github.com>
2022-09-17 23:23:20 +0100

    final logo 'brain' with css

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,25 +1,30 @@
 import React from "react";
 import Tilt from 'react-parallax-tilt';
 import styled from 'styled-components';
+import brain from './brain.png';
 
 
 
 const Logo = () => {
     return (
-<LogoDiv className="ma4 mt0">
- <Tilt>
-      <div style={{ height: '150px', width: '150px', backgroundColor: 'transparent' }}>
-        <h1> 👀</h1>
+<div className="ma4 mt0">
+ <Tilt style={{ height: '100px', width: '100px', backgroundColor: 'transparent', border: '1px solid white', borderRadius: '20px' }}>
+      <div className="pa3">
+       <img src={brain} alt='brainlogo'></img>
       </div>
  </Tilt>
-</LogoDiv>
+</div>
     )
 };
 
-const LogoDiv = styled.div`
-background: linear-gradient(89deg, #FF5EDF 0%, #04C8DE 100%);
-border: 1px solid white;
-border-radius: 20px;
-width:150px;
-`;
+// const LogoDiv = styled.div`
+// background: linear-gradient(89deg, #FF5EDF 0%, #04C8DE 100%);
+// border: 1px solid white;
+// border-radius: 20px;
+// width:100px;
+
+// img {
+//   padding:35px;
+// }
+// `;
 export default Logo;
