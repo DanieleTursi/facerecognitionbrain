fa2603fe703ff62cc19e583296d2f0a6de03ccd0
DanieleTursi <99504866+DanieleTursi@users.noreply.github.com>
2022-09-19 15:11:13 +0100

    rank component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+           
      <Navigation />
      <Logo />
+     <Rank />
      <ImageLinkForm/>
      {/* {
         
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,8 +7,8 @@ import brain from './brain.png';
 
 const Logo = () => {
     return (
-<div className="ma4 mt0">
- <Tilt style={{ height: '100px', width: '100px', backgroundColor: 'transparent', border: '1px solid white', borderRadius: '10px',background: 'linear-gradient(89deg, #FF5EDF 0%, #04C8DE 100%)' }}>
+<div className="ma4 mt0 ">
+ <Tilt  style={{ height: '100px', width: '100px', backgroundColor: 'transparent', border: '1px solid white', borderRadius: '10px',background: 'linear-gradient(89deg, #FF5EDF 0%, #04C8DE 100%)' }}>
       <div className="pa3">
        <img src={brain} alt='brainlogo'></img>
       </div>
