70d194b5b3e41fdaff2b749f5e34b7a7a4d74c31
DanieleTursi <99504866+DanieleTursi@users.noreply.github.com>
2022-09-20 18:30:15 +0100

    background react particles

--- src/App.js
+++ src/App.js
@@ -1,35 +1,110 @@
-import React from 'react';
-import ParticlesBackground from './components/ParticlesBackground/ParticlesBackground';
+import React, { useCallback } from 'react';
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import './index.css';
 import './App.css';
 
 
-const particlesOptions = {
-  particles: {
-    number: {
-      value: 30,
-      density: {
-        enable: true,
-        value_area: 800
-      }
-    }
-  }
-}
+
 
 function App() {
+  const particlesInit = useCallback(async (engine) => {
+     
+    await loadFull(engine);
+}, []);
+
+const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+}, []);
+
   return (
     <div className="App">
-       {/* <Particles className='particles'
-          params={particlesOptions}
-        /> */}
-        <ParticlesBackground />
+      
      <Navigation />
      <Logo />
      <Rank />
      <ImageLinkForm/>
+     <Particles className='zIndex'
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    color: {
+                        value: "#0d47a1",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        directions: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 6,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
      {/* {
         
         <FaceRecognition />} */}
