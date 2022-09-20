675caa0384267391249a46788927b26fe47bd79b
DanieleTursi <99504866+DanieleTursi@users.noreply.github.com>
2022-09-20 17:35:44 +0100

    tsparticles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import ParticlesBackground from './components/ParticlesBackground/ParticlesBackground';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
-           
+       {/* <Particles className='particles'
+          params={particlesOptions}
+        /> */}
+        <ParticlesBackground />
      <Navigation />
      <Logo />
      <Rank />
