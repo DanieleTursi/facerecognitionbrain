52b4ff011ab717deab5d1b6700b78fc67ee66a2f
DanieleTursi <99504866+DanieleTursi@users.noreply.github.com>
2022-09-22 19:39:53 +0100

    api clarifai

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { useCallback, Component } from 'react';
 import Particles from "react-particles";
+import Clarifai from 'clarifai';
 import { loadFull } from "tsparticles";
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './index.css';
 import './App.css';
+import { render } from '@testing-library/react';
 
 
-
+const app = new Clarifai.App({
+    apiKey: 'f098004c61634e5392f87d487717f17b'
+   });
 
 function App() {
   const particlesInit = useCallback(async (engine) => {
@@ -28,21 +33,54 @@ class App extends Component {
             input:'',
         }
     }
-  
-}
+    
+    onInputChange = (event) => {
+        this.setState({input: event.target.value});
+      }
 
-onInputChange = (event) => {
-    this.setState({input: event.target.value});
-  }
-  
+      onButtonSubmit = () => {
+        this.setState({imageUrl: this.state.input});
+        app.models
+          .predict(
+        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+        // is to use a different version of their model that works like the ones found here: https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
+        // so you would change from:
+        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+        // to:
+        // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
+            Clarifai.FACE_DETECT_MODEL,
+            this.state.input)
+          .then(response => {
+            console.log('hi', response)
+            if (response) {
+              fetch('http://localhost:3000/image', {
+                method: 'put',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                  id: this.state.user.id
+                })
+              })
+                .then(response => response.json())
+                .then(count => {
+                  this.setState(Object.assign(this.state.user, { entries: count}))
+                })
+    
+            }
+            this.displayFaceBox(this.calculateFaceLocation(response))
+          })
+          .catch(err => console.log(err));
+      }
+
+render(){
   return (
     <div className="App">
-
-      
      <Navigation />
      <Logo />
      <Rank />
-     <ImageLinkForm onInputChange={this.onInputChange}/>
+     <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
      <Particles className='zIndex'
             id="tsparticles"
             init={particlesInit}
@@ -120,11 +158,10 @@ onInputChange = (event) => {
                 detectRetina: true,
             }}
         />
-     {/* {
-        
-        <FaceRecognition />} */}
+     { <FaceRecognition />}
     </div>
   );
+  }
 }
-
+}
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,7 +4,7 @@ import './ImageLinkForm.css';
 
 
 
-const ImageLinkForm = ({onInputChange}) => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return (
 <div>
     <p className="f3">
@@ -13,7 +13,8 @@ const ImageLinkForm = ({onInputChange}) => {
     <div className="center">
         <div className="pa4 br3 shadow-5 form center">
         <input className='f4 pa2 w-70 center' type="tex" onChange={onInputChange}/>
-        <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+        <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+        onClick={onButtonSubmit}>Detect</button>
         </div>
     </div>
 </div>
